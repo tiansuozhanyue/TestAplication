0f5ba5baf7b5db89070bacb34bbefe7f0ca56a1d
tiansuozhanyue <18868026919@163.cpm>
2017-10-30 16:47:26 +0800

    Initial commit

---
+++ githublibrary/src/main/java/com/example/githublibrary/TestUtils.java
@@ -0,0 +1,16 @@
+package com.example.githublibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2017/10/30.
+ */
+
+public class TestUtils {
+
+    public void showTest(Context context) {
+        Toast.makeText(context, "test", Toast.LENGTH_SHORT);
+    }
+
+}
